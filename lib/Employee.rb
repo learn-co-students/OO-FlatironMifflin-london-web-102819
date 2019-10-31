a4c7ce42a367d438634b9da490577b47482f25bc
github username <github email address>
2019-10-31 16:54:50 +0000

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,32 @@
-class Employee
+class Employees
+
+    attr_accessor :name, :salary, :department, :manager
+    @@all = []
+
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over
+        @@all.select{|employee| employee.salary > 1000}
+    end
+
+    def tax_bracket
+        @all.select{|employee| employee.salary < self.salary + 1000 && employee.salary > self.salary - 1000}
+    end
+
+    def manager_name
+        @manager.name
+    end
+
+    def self.find_by_department(department_name)
+        @@all.find{|employee| employee.department == department_name}
+    end
 
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,38 @@
 class Manager
 
+    attr_accessor :name, :age
+    @@all = []
+
+    def initialize(name, department, age)
+        @name = name
+        @department = department
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.average_age
+        Manager.all.map{|m| m.age}.sum / @@all.count.to_f
+    end
+
+    def employees
+        Employee.all.select{|employee| employee.department = self.department}
+    end
+
+    def self.all_departments
+        @@all.map{|manager| manager.department}.uniq
+    end
+
+    def hire_employee(employee_name, salary)
+        Employee.all.each do |employee|
+            if employee.name == employee_name && employee.salary == salary
+                employee.manager = self
+                return employee
+            end
+        end
+    end
+
 end
