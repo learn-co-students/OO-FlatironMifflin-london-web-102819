dd445e1f9329507944ce890a2b67630937449196
Qing <chingwang.qw@gmail.com>
2019-11-01 11:43:47 +0000

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,32 @@
 class Manager
+  attr_reader :name, :department
+  attr_accessor :age
 
+  @@all = []
+  def initialize(name, department, age)
+    @name
+    @department = department
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def hire_employee(employee_name, salary)
+    Employee.new(employee_name, self, salary)
+  end
+
+  def self.all_departments
+    Manager.all.map {|m| m.department} 
+  end
+
+  def self.all_ages
+    Manager.all.map {|m| m.age}
+  end
+
+  def self.average_age
+    all_ages.sum / all_ages.size.to_f
+  end
 end
