5cd8fe71c50aba6c6e10662c57f5e43d37db3bbc
hiroshiuk1991 <hiroshis91@icloud.com>
2019-11-03 15:46:20 +0000

    Done.

--- run.rb
+++ run.rb
@@ -4,6 +4,14 @@ require 'pry'
 
 
 #Test your code here
+m1 = Manager.new("Michael Scott", "Regional Manager", 42 )
+m2 = Manager.new("David Wallace", "Group Manager", 48 )
+
+
+e1 = Employee.new("Jim", m1, 2000)
+e2 = Employee.new("Pam", m2, 3000)
+e3 = Employee.new("Dwight", m1, 1000)
+e4 = Employee.new("Andy", m2, 400)
 
 
 binding.pry
