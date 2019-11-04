a51807f80152ef386b331473ad04f99100f10c50
github username <harr.hughes@gmail.com>
2019-11-04 11:54:11 +0000

    first attempt

--- run.rb
+++ run.rb
@@ -2,9 +2,12 @@ require_relative "lib/Manager"
 require_relative "lib/Employee"
 require 'pry'
 
+anna = Manager.new('Anna', 'advertising', 35)
+lucy = Manager.new('Lucy', 'accounting', 47)
 
-#Test your code here
-
+mike = Employee.new('Mike', 5000, anna)
+jeff = Employee.new('Jeff', 3000, anna)
+phil = Employee.new('Phil', 6000, lucy)
 
 binding.pry
 puts "done"
