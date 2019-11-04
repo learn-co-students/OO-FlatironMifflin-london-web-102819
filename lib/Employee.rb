fd431fafaa5eee23fe5a40a496031cc4cee1e0b4
andrew-lewell <andrew.lewell@gmail.com>
2019-11-04 12:17:19 +0000

    done!

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,46 @@
 class Employee
 
+    attr_reader :name
+    attr_accessor :salary, :manager
+
+    @@all = []
+
+    def initialize(name, salary, manager)
+        @name = name 
+        @salary = salary 
+        @manager = manager
+        @@all << self
+    end 
+
+    def name 
+        @name 
+    end
+
+    def salary 
+        @salary 
+    end 
+
+    def manager_name 
+        @manager.name
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def self.paid_over(base)
+        all.select { |employee| employee.salary > base }
+    end 
+
+    def self.find_by_department(dept)
+        manager = Manager.all.find { |manager| manager.department == dept}
+        manager.employees.find { |e| e }
+    end 
+
+    def tax_bracket 
+        salary_high = @salary + 1000
+        salary_low = @salary - 1000
+        Employee.all.select { |employee| employee.salary <= salary_high && employee.salary >= salary_low }
+    end
+
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,48 @@
 class Manager
 
+    attr_accessor :name, :department, :age 
+    
+    @@all = [] 
+
+    def initialize(name, department, age)
+        @name = name 
+        @department = department
+        @age = age 
+        @@all << self
+    end 
+
+    def name 
+        @name
+    end
+
+    def age 
+        @age
+    end 
+
+    def employees
+        Employee.all.select { |employee| employee.manager == self }
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
+    def hire_employee(name, salary)
+        new_hire = Employee.new(name, salary, self)
+    end
+
+    def self.all_departments
+        all.map { |manager| manager.department }
+    end 
+
+    def self.manager_ages   # returns array of Manager ages
+        all.map { |manager| manager.age }
+    end
+
+    def self.average_age
+        manager_count = all.count 
+        total_age = manager_ages.reduce(:+)
+        (total_age / manager_count).to_f 
+    end 
+
 end
